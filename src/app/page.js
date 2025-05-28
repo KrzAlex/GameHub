5401475bc57590c232912cc0610f25f29270730d
KrzAlex <aledeac777@gmail.com>
2025-05-28 18:39:52 +0300

    supabase added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { supabase } from '../../lib/supabaseClient'
+
+/*
+export default async function handler(req, res) {
+  const { data, error } = await supabase.from('games').select('*')
+  if (error) return res.status(500).json({ error })
+  res.status(200).json(data)
+}*/
 
 export default function Home() {
   return (
